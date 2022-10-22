368f3838776409c9749dfc149fb621b07437f7df
Tristan Brigham <67669475+TristanB22@users.noreply.github.com>
2022-10-22 09:32:42 -0400

    Removing jammy file

--- host_scripts/script.sh
+++ host_scripts/script.sh
@@ -2,10 +2,12 @@ sudo apt-get install cloud-image-utils
 
 cloud-localds user-data.img user-data
 
-# wget https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img -P ~
+mkdir /home/qemu_files
 
-qemu-img resize ~/jammy-server-cloudimg-amd64.img +128G
+# wget https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img -P /home/qemu_files
 
-qemu-system-x86_64 -machine accel=kvm -cpu host -drive file=~/jammy-server-cloudimg-amd64.img,format=qcow2 -drive file=user-data.img,format=raw -m 8G -display none -monitor stdio -net nic,model=e1000 -net user,hostfwd=tcp::2222-:22 -smp cores=14,threads=1,sockets=1
+qemu-img resize /home/qemu_files/jammy-server-cloudimg-amd64.img +128G
+
+qemu-system-x86_64 -machine accel=kvm -cpu host -drive file=/home/qemu_files/jammy-server-cloudimg-amd64.img,format=qcow2 -drive file=user-data.img,format=raw -m 8G -display none -monitor stdio -net nic,model=e1000 -net user,hostfwd=tcp::2222-:22 -smp cores=14,threads=1,sockets=1
 
 
