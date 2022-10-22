213c1208215fe03dc3fd6f5d226cd029a41c3c31
Tristan Brigham <67669475+TristanB22@users.noreply.github.com>
2022-10-22 10:01:27 -0400

    Updating install script

--- host_scripts/ssh_command.sh
+++ host_scripts/ssh_command.sh
@@ -1,2 +1,2 @@
 sudo ssh-keygen -f "/root/.ssh/known_hosts" -R "[localhost]:2222"
-ssh -p 2222 ubuntu@localhost
+ssh -p 2222 ubuntu@localhost "git clone https://github.com/Lind-Project/pop-paths-scripts.git && "
