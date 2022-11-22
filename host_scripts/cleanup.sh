30852d500a1a5e0c727c39345d4843364686fe52
Tristan Brigham <67669475+TristanB22@users.noreply.github.com>
2022-11-22 09:29:54 -0700

    Updating script with if command to download img

--- host_scripts/cleanup.sh
+++ host_scripts/cleanup.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 rm /home/tbrigham/jammy-server-cloudimg-amd64.img
 rm user-data.img
 sudo apt remove -y cloud-image-utils
--- host_scripts/script.sh
+++ host_scripts/script.sh
@@ -2,8 +2,9 @@ sudo apt-get install cloud-image-utils
 
 cloud-localds user-data.img user-data
 
-if [$1 -eq 1]
+if [ $1 -eq 1 ]
 then
+    echo "DOWNLOADING"
     wget https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img -P /home/tbrigham
 fi
 
