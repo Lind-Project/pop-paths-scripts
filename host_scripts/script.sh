46fd85a34596ba01cc129df1ed3d19f7dfddaabb
Tristan Brigham <67669475+TristanB22@users.noreply.github.com>
2022-11-22 17:11:13 -0700

    Updating ssh commands

--- host_scripts/script.sh
+++ host_scripts/script.sh
@@ -11,4 +11,3 @@ fi
 qemu-img resize /home/tbrigham/jammy-server-cloudimg-amd64.img +128G
 
 qemu-system-x86_64 -machine accel=kvm -cpu host -drive file=/home/tbrigham/jammy-server-cloudimg-amd64.img,format=qcow2 -drive file=user-data.img,format=raw -m 8G -display none -monitor stdio -net nic,model=e1000 -net user,hostfwd=tcp::2222-:22 -smp cores=14,threads=1,sockets=1
-
--- host_scripts/ssh_command.sh
+++ host_scripts/ssh_command.sh
@@ -1,5 +1,11 @@
 sudo ssh-keygen -f "/home/tbrigham/.ssh/known_hosts" -R "[localhost]:2222"
-# sudo ssh-keygen -f "/root/.ssh/known_hosts" -R "[localhost]:2222"
+sudo ssh-keygen -f "/root/.ssh/known_hosts" -R "[localhost]:2222"
 ssh -p 2222 ubuntu@localhost "git clone https://github.com/Lind-Project/pop-paths-scripts.git && bash pop-paths-scripts/pop_paths_scripts/script1.sh"
+
+sudo ssh-keygen -f "/home/tbrigham/.ssh/known_hosts" -R "[localhost]:2222"
+sudo ssh-keygen -f "/root/.ssh/known_hosts" -R "[localhost]:2222"
 ssh -p 2222 ubuntu@localhost "sudo bash pop-paths-scripts/pop_paths_scripts/script2.sh"
+
+sudo ssh-keygen -f "/home/tbrigham/.ssh/known_hosts" -R "[localhost]:2222"
+sudo ssh-keygen -f "/root/.ssh/known_hosts" -R "[localhost]:2222"
 ssh -p 2222 ubuntu@localhost "sudo bash pop-paths-scripts/pop_paths_scripts/script3.sh"
