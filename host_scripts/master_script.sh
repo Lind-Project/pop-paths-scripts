eae9c45ceffaf072228ac87dc28ae55373e3f38b
Tristan Brigham <67669475+TristanB22@users.noreply.github.com>
2022-11-23 08:58:10 -0700

    Updating script structure

--- host_scripts/master_script.sh
+++ host_scripts/master_script.sh
@@ -1,13 +1,10 @@
-sudo bash script.sh $1 &
-(
-    if [ $1 = 1 ] ; then 
-        sleep 30
-    else
-        sleep 10
-    fi
-    sudo ./ssh.exp 1
+if [ $1 = 1 ] ; then 
+    sleep 30
+else
     sleep 10
-    sudo ./ssh.exp 2
-    sleep 10
-    sudo ./ssh.exp 3
-)
+fi
+sudo ./ssh.exp 1
+sleep 10
+sudo ./ssh.exp 2
+sleep 10
+sudo ./ssh.exp 3
