2100f6c83a0ccaa93fd9b2248417f0d08eab5f72
Tristan Brigham <67669475+TristanB22@users.noreply.github.com>
2022-11-23 09:39:27 -0700

    Updating expect

--- host_scripts/master_script.sh
+++ host_scripts/master_script.sh
@@ -5,12 +5,12 @@
 # fi
 
 echo "SSHING 1"
-sudo sshpass -p "password" ssh -o StrictHostKeyChecking=no ubuntu@localhost:2222 "git clone https://github.com/Lind-Project/pop-paths-scripts.git && bash pop-paths-scripts/pop_paths_scripts/script1.sh" && 
+sudo sshpass -p "password" ssh -o StrictHostKeyChecking=no -p 2222 ubuntu@localhost "git clone https://github.com/Lind-Project/pop-paths-scripts.git && bash pop-paths-scripts/pop_paths_scripts/script1.sh" && 
 sleep 10 &&
 
 echo "SSHING 2"
-sudo sshpass -p "password" ssh -o StrictHostKeyChecking=no ubuntu@localhost:2222 "sudo bash pop-paths-scripts/pop_paths_scripts/script2.sh" && 
+sudo sshpass -p "password" ssh -o StrictHostKeyChecking=no -p 2222 ubuntu@localhost "sudo bash pop-paths-scripts/pop_paths_scripts/script2.sh" && 
 sleep 10 &&
 
 echo "SSHING 3"
-sudo sshpass -p "password" ssh -o StrictHostKeyChecking=no ubuntu@localhost:2222 "sudo bash pop-paths-scripts/pop_paths_scripts/script3.sh"
+sudo sshpass -p "password" ssh -o StrictHostKeyChecking=no -p 2222 ubuntu@localhost "sudo bash pop-paths-scripts/pop_paths_scripts/script3.sh"
