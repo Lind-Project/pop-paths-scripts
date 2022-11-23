8e4e33828fdb17f53091882236db205fc0dcd776
Tristan Brigham <67669475+TristanB22@users.noreply.github.com>
2022-11-23 08:48:19 -0700

    Updating ssh commands

--- host_scripts/ssh_command.sh
+++ host_scripts/ssh_command.sh
@@ -3,15 +3,21 @@ if [ $1 -eq 1 ]
 then
     sudo ssh-keygen -f "/home/tbrigham/.ssh/known_hosts" -R "[localhost]:2222"
     sudo ssh-keygen -f "/root/.ssh/known_hosts" -R "[localhost]:2222"
-    ssh -p 2222 ubuntu@localhost "git clone https://github.com/Lind-Project/pop-paths-scripts.git && bash pop-paths-scripts/pop_paths_scripts/script1.sh"
+    until ssh -p 2222 ubuntu@localhost "git clone https://github.com/Lind-Project/pop-paths-scripts.git && bash pop-paths-scripts/pop_paths_scripts/script1.sh"; do
+        sleep 5
+    done
 elif [ $1 -eq 2 ]
 then
     sudo ssh-keygen -f "/home/tbrigham/.ssh/known_hosts" -R "[localhost]:2222"
     sudo ssh-keygen -f "/root/.ssh/known_hosts" -R "[localhost]:2222"
-    ssh -p 2222 ubuntu@localhost "sudo bash pop-paths-scripts/pop_paths_scripts/script2.sh"
+    until ssh -p 2222 ubuntu@localhost "sudo bash pop-paths-scripts/pop_paths_scripts/script2.sh"; do
+        sleep 5
+    done
 elif [ $1 -eq 3 ]
 then
     sudo ssh-keygen -f "/home/tbrigham/.ssh/known_hosts" -R "[localhost]:2222"
     sudo ssh-keygen -f "/root/.ssh/known_hosts" -R "[localhost]:2222"
-    ssh -p 2222 ubuntu@localhost "sudo bash pop-paths-scripts/pop_paths_scripts/script3.sh"
+    until ssh -p 2222 ubuntu@localhost "sudo bash pop-paths-scripts/pop_paths_scripts/script3.sh"; do
+        sleep 5
+    done
 fi
