9d28546a7c593f4376e7818813e0eecfdc0c08fc
Tristan Brigham <67669475+TristanB22@users.noreply.github.com>
2022-11-29 11:01:40 -0500

    testing scp on server

--- host_scripts/scripts_for_ssh/ssh_command.sh
+++ host_scripts/scripts_for_ssh/ssh_command.sh
@@ -32,7 +32,7 @@ then
     sudo ssh-keygen -f "/root/.ssh/known_hosts" -R "[localhost]:2222"
     echo "HIT HERE"
     # until sudo sshpass -p "password" ssh -o StrictHostKeyChecking=no -p 2222 ubuntu@localhost "sudo bash pop-paths-scripts/pop_paths_scripts/script3.sh"; do
-    sudo sshpass -p "password" sudo scp -P 2222 -r 'ubuntu@localhost:/home/ubuntu/GCOV_DATA /hdd/GCOV_DATA/$(date --iso-8601="minutes")'
+    sudo sshpass -p "password" sudo scp -P 2222 -r 'ubuntu@localhost:/home/ubuntu/GCOV_DATA' '/hdd/GCOV_DATA/$(date --iso-8601="minutes")'
     #     sleep 10
     # done
 fi
