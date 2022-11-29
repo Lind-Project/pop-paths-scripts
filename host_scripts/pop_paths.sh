971de4931196bf1686faf58d13432ac5421e9a40
Tristan Brigham <67669475+TristanB22@users.noreply.github.com>
2022-11-29 10:37:50 -0500

    Adding scp

--- host_scripts/pop_paths.sh
+++ host_scripts/pop_paths.sh
@@ -3,3 +3,8 @@ echo "SSHING 3"
 echo ""
 echo ""
 sudo bash scripts_for_ssh/ssh_command.sh 3
+echo ""
+echo "RETRIEVING FOLDERS"
+echo ""
+echo ""
+sudo bash scripts_for_ssh/ssh_command.sh 4
--- host_scripts/scripts_for_ssh/ssh_command.sh
+++ host_scripts/scripts_for_ssh/ssh_command.sh
@@ -25,4 +25,13 @@ then
     sudo sshpass -p "password" ssh -o StrictHostKeyChecking=no -p 2222 ubuntu@localhost "sudo bash pop-paths-scripts/pop_paths_scripts/script3.sh"
     #     sleep 10
     # done
+elif [ $1 -eq 4 ]
+then
+    sleep 5
+    sudo ssh-keygen -f "/home/tbrigham/.ssh/known_hosts" -R "[localhost]:2222"
+    sudo ssh-keygen -f "/root/.ssh/known_hosts" -R "[localhost]:2222"
+    # until sudo sshpass -p "password" ssh -o StrictHostKeyChecking=no -p 2222 ubuntu@localhost "sudo bash pop-paths-scripts/pop_paths_scripts/script3.sh"; do
+    sudo sshpass -p "password" scp -P 2222 -r ubuntu@localhost:/home/ubuntu/GCOV_DATA /hdd/GCOV_DATA/$(date)
+    #     sleep 10
+    # done
 fi
