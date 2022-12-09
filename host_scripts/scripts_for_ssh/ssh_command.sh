5229d66254b9fd9ac870accddf627c0727189dde
Tristan Brigham <67669475+TristanB22@users.noreply.github.com>
2022-12-09 14:46:51 -0500

    Updating ssh expect scripts

--- host_scripts/scripts_for_ssh/ssh_command.sh
+++ host_scripts/scripts_for_ssh/ssh_command.sh
@@ -19,7 +19,6 @@ then
     # done
 elif [ $1 -eq 3 ]
 then
-    sleep 30
     sudo ssh-keygen -f "/home/tbrigham/.ssh/known_hosts" -R "[localhost]:2222"
     sudo ssh-keygen -f "/root/.ssh/known_hosts" -R "[localhost]:2222"
     # until sudo sshpass -p "password" ssh -o StrictHostKeyChecking=no -p 2222 ubuntu@localhost "sudo bash pop-paths-scripts/pop_paths_scripts/script3.sh"; do
