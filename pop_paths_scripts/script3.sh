c18a51546c8aa04d449bcd99d69e5fff613a3a0d
Tristan Brigham <67669475+TristanB22@users.noreply.github.com>
2022-12-10 15:43:18 -0500

    Updating sudo permissions

--- pop_paths_scripts/script3.sh
+++ pop_paths_scripts/script3.sh
@@ -11,12 +11,12 @@ mkdir GCOV_DATA/pop_paths
 mkdir GCOV_DATA/full_data
 
 #resetting the data
-# sudo echo 0 > /sys/kernel/debug/gcov/reset
-# sudo bash pop-paths-scripts/pop_paths_scripts/makeReport.sh GCOV_DATA/blank_reset
-# sudo python3 pop-paths-scripts/pop_paths_scripts/run_pop_paths.py 2 #still need to do some automation here
+sudo echo 0 > /sys/kernel/debug/gcov/reset
+sudo bash pop-paths-scripts/pop_paths_scripts/makeReport.sh GCOV_DATA/blank_reset
+sudo python3 pop-paths-scripts/pop_paths_scripts/run_pop_paths.py 2 #still need to do some automation here
 
-# #collecting the pop paths data
-# sudo bash pop-paths-scripts/pop_paths_scripts/makeReport.sh ./GCOV_DATA/pop_paths
+#collecting the pop paths data
+sudo bash pop-paths-scripts/pop_paths_scripts/makeReport.sh ./GCOV_DATA/pop_paths
 
 #trinity and ltp here
 timeout 6h bash pop-paths-scripts/pop_paths_scripts/run_trinity.sh 
