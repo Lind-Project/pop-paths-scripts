ed91e534b17dc635b85bf49df55994b7d467c22c
Tristan Brigham <67669475+TristanB22@users.noreply.github.com>
2022-12-10 15:19:28 -0500

    Updating sudo permissions

--- host_scripts/scripts_for_ssh/ssh_command.sh
+++ host_scripts/scripts_for_ssh/ssh_command.sh
@@ -14,7 +14,7 @@ then
     sudo ssh-keygen -f "/home/tbrigham/.ssh/known_hosts" -R "[localhost]:2222"
     sudo ssh-keygen -f "/root/.ssh/known_hosts" -R "[localhost]:2222"
     # until sudo sshpass -p "password" ssh -o StrictHostKeyChecking=no -p 2222 ubuntu@localhost "sudo bash pop-paths-scripts/pop_paths_scripts/script2.sh"; do
-    sudo ssh -i /home/tbrigham/key -o StrictHostKeyChecking=no -p 2222 ubuntu@localhost "sudo bash pop-paths-scripts/pop_paths_scripts/script2.sh"
+    sudo ssh -i /home/tbrigham/key -o StrictHostKeyChecking=no -p 2222 ubuntu@localhost "bash pop-paths-scripts/pop_paths_scripts/script2.sh"
     #     sleep 10
     # done
 elif [ $1 -eq 3 ]
@@ -22,7 +22,7 @@ then
     sudo ssh-keygen -f "/home/tbrigham/.ssh/known_hosts" -R "[localhost]:2222"
     sudo ssh-keygen -f "/root/.ssh/known_hosts" -R "[localhost]:2222"
     # until sudo sshpass -p "password" ssh -o StrictHostKeyChecking=no -p 2222 ubuntu@localhost "sudo bash pop-paths-scripts/pop_paths_scripts/script3.sh"; do
-    sudo ssh -i /home/tbrigham/key -o StrictHostKeyChecking=no -p 2222 ubuntu@localhost "sudo bash pop-paths-scripts/pop_paths_scripts/script3.sh"
+    sudo ssh -i /home/tbrigham/key -o StrictHostKeyChecking=no -p 2222 ubuntu@localhost "bash pop-paths-scripts/pop_paths_scripts/script3.sh"
     #     sleep 10
     # done
 elif [ $1 -eq 4 ]
