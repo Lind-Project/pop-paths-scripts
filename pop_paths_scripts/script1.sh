a2c343c609c63b9335c4514c1661729b71fd30ff
Tristan Brigham <67669475+TristanB22@users.noreply.github.com>
2022-12-09 19:30:38 -0500

    Updating client timeout

--- pop_paths_scripts/script1.sh
+++ pop_paths_scripts/script1.sh
@@ -1,7 +1,10 @@
 sudo apt-get update
 
-#installing packages
+# installing packages
 sudo bash pop-paths-scripts/pop_paths_scripts/install_script.sh
 
-#disabling App Armor
+# updating the timeout values
+cp pop-paths-scripts/pop_paths_scripts/extra_files/sshd_config /etc/ssh/sshd_config
+
+# disabling App Armor
 sudo python3 pop-paths-scripts/pop_paths_scripts/run_pop_paths.py 0
