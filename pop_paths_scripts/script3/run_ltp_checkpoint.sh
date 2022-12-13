4e85154141156e15aa44f842fd6d1e3e17c0b62f
Tristan Brigham <67669475+TristanB22@users.noreply.github.com>
2022-12-12 21:24:43 -0500

    Updating ltp timing

--- pop_paths_scripts/script3/run_ltp_checkpoint.sh
+++ pop_paths_scripts/script3/run_ltp_checkpoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # run ltp
-sudo timeout 6h bash pop-paths-scripts/pop_paths_scripts/run_ltp.sh 
+sudo timeout 2h bash pop-paths-scripts/pop_paths_scripts/run_ltp.sh 
 
 # generate report
 sudo bash pop-paths-scripts/pop_paths_scripts/makeReport.sh ./GCOV_DATA/full_data
