cc939e8b2c77a31797dfa39581ba9362929ccc16
Tristan Brigham <67669475+TristanB22@users.noreply.github.com>
2022-12-13 11:55:42 -0500

    deleting kernel copy after installing it to free space

--- pop_paths_scripts/script2.sh
+++ pop_paths_scripts/script2.sh
@@ -20,6 +20,8 @@ wget https://launchpad.net/ubuntu/+archive/primary/+sourcefiles/linux/5.11.0-49.
 tar -xvf linux_5.11.0.orig.tar.gz 
 cd linux-5.11/
 sudo apt-get install --assume-yes libncurses-dev flex bison openssl libssl-dev dkms libelf-dev libudev-dev libpci-dev libiberty-dev autoconf lcov expect
+
+# making the kernel
 timeout 5s make menuconfig
 sudo cp ../pop-paths-scripts/pop_paths_scripts/extra_files/BIG_CONFIG.config ./.config
 sudo make -j 4
@@ -27,6 +29,9 @@ sudo make modules_install
 sudo make install
 cd ..
 sudo cp pop-paths-scripts/pop_paths_scripts/extra_files/grub_in /etc/default/grub
+
+# deleting the kernel file (because it is obscenely large)
+sudo rm -r /home/ubuntu/linux-5.11
 
 #enabling Selinux
 sudo python3 pop-paths-scripts/pop_paths_scripts/run_pop_paths.py 1
