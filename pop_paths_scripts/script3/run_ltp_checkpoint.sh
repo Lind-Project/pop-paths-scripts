8542ee42519504172e16a92fa706768528945bb5
Tristan Brigham <67669475+TristanB22@users.noreply.github.com>
2022-12-22 07:59:38 -0700

    Removing log for ls

--- pop_paths_scripts/script3/run_ltp_checkpoint.sh
+++ pop_paths_scripts/script3/run_ltp_checkpoint.sh
@@ -3,9 +3,5 @@
 # run ltp
 sudo timeout 2h bash pop-paths-scripts/pop_paths_scripts/run_ltp.sh 
 
-ls -ahl /hdd > /home/tbrigham/log1.txt
-
 # generate report
 sudo bash pop-paths-scripts/pop_paths_scripts/makeReport.sh ./GCOV_DATA/full_data
-
-ls -ahl /hdd > /home/tbrigham/log1.txt
--- pop_paths_scripts/script3/run_trinity_checkpoint.sh
+++ pop_paths_scripts/script3/run_trinity_checkpoint.sh
@@ -3,8 +3,6 @@
 #trinity here
 timeout 6h bash pop-paths-scripts/pop_paths_scripts/run_trinity.sh 
 
-ls -ahl /hdd > /home/tbrigham/log1.txt
-
 # call another script that runs ltp and then calls other scripts
 # so that if we fail during trinity, we can easily pick back up where we left off
 # by running the command in the sequence again
