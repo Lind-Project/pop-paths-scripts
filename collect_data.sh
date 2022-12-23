f49f73bdcb6d5674aeea79872ee0f6066adf3971
Tristan Brigham <67669475+TristanB22@users.noreply.github.com>
2022-12-23 15:22:02 -0700

    Updating Master run file paths

--- collect_data.sh
+++ collect_data.sh
@@ -8,11 +8,11 @@ echo "Done"
 # server
 echo "Init Server..."
 screen -mdS "server_screen"
-screen -S "server_screen" -p 0 -X stuff "cd pop-paths-scripts/host_scripts && echo $1 | sudo -S  bash ./server.sh 1 ^M"
+screen -S "server_screen" -p 0 -X stuff "cd host_scripts && echo $1 | sudo -S  bash ./server.sh 1 ^M"
 echo "Done"
 
 # client
 echo "Init Server..."
 screen -mdS "client_screen"
-screen -S "client_screen" -p 0 -X stuff "cd pop-paths-scripts/host_scripts && echo $1 | sudo -S  bash ./setup.sh && echo $1 | sudo -S  bash ./pop_paths.sh ^M"
+screen -S "client_screen" -p 0 -X stuff "cd host_scripts && echo $1 | sudo -S  bash ./setup.sh && echo $1 | sudo -S  bash ./pop_paths.sh ^M"
 echo "Done"
