51b1cb8af41bb84f01f73c437ff08ce4600b3757
Tristan Brigham <67669475+TristanB22@users.noreply.github.com>
2022-12-23 15:31:10 -0700

    Updating Master run file permissions

--- collect_data.sh
+++ collect_data.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+########################################################
+########            LIND PROJECT                ########
+########      Pop-paths data collector          ########
+########           Tristan Brigham              ########
+########################################################
+
+# to run this program, run it in bash with sudo permissions
+# it requires your password as the first command line argument
+# to set up the screens
+
+# to monitor progress, run one of the following commands 
+
 # clean up
 echo "Init Environment..."
 sudo rm -f /hdd/jammy-server-cloudimg-amd64.img  || true
@@ -8,7 +20,7 @@ echo "Done"
 # server
 echo "Init Server..."
 screen -mdS "server_screen"
-screen -S "server_screen" -p 0 -X stuff "cd host_scripts && echo $1 | sudo -S  bash ./server.sh 1 ^M"
+screen -S "server_screen" -p 0 -X stuff "cd host_scripts && sudo bash ./server.sh 1 ^M"
 echo "Done. See info below"
 sudo ls -laR /var/run/screen | grep "server"
 
