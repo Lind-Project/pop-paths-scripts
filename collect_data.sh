f42ca69801d0d082b4c8cace03bf46bafef86a96
Tristan Brigham <67669475+TristanB22@users.noreply.github.com>
2022-12-23 15:18:27 -0700

    Creating Master run file

--- collect_data.sh
+++ collect_data.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
 # clean up
+echo "Init Environment..."
 sudo rm -f /hdd/jammy-server-cloudimg-amd64.img  || true
+echo "Done"
 
 # server
+echo "Init Server..."
 screen -mdS "server_screen"
 screen -S "server_screen" -p 0 -X stuff "cd pop-paths-scripts/host_scripts && echo $1 | sudo -S  bash ./server.sh 1 ^M"
+echo "Done"
 
 # client
+echo "Init Server..."
 screen -mdS "client_screen"
 screen -S "client_screen" -p 0 -X stuff "cd pop-paths-scripts/host_scripts && echo $1 | sudo -S  bash ./setup.sh && echo $1 | sudo -S  bash ./pop_paths.sh ^M"
-
-# clean up after ourselves
-sudo rm -f /hdd/jammy-server-cloudimg-amd64.img  || true
+echo "Done"
