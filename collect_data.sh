35709357f3110887ec5c297b6da77555b58a6561
Tristan Brigham <67669475+TristanB22@users.noreply.github.com>
2022-12-23 15:25:19 -0700

    Updating Master run file paths

--- collect_data.sh
+++ collect_data.sh
@@ -9,10 +9,13 @@ echo "Done"
 echo "Init Server..."
 screen -mdS "server_screen"
 screen -S "server_screen" -p 0 -X stuff "cd host_scripts && echo $1 | sudo -S  bash ./server.sh 1 ^M"
-echo "Done"
+echo "Done. See info below"
+sudo ls -laR /var/run/screen | grep "server"
 
 # client
 echo "Init Server..."
 screen -mdS "client_screen"
 screen -S "client_screen" -p 0 -X stuff "sleep 100 && cd host_scripts && echo $1 | sudo -S  bash ./setup.sh && echo $1 | sudo -S  bash ./pop_paths.sh ^M"
 echo "Done"
+sudo ls -laR /var/run/screen | grep "client"
+
