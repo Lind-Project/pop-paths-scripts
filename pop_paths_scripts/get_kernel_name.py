32bab3d9ae3c47556bc8fd17ac548e1cbb2a8365
Tristan Brigham <67669475+TristanB22@users.noreply.github.com>
2023-01-12 18:30:50 -0700

    get kernel name

--- pop_paths_scripts/get_kernel_name.py
+++ pop_paths_scripts/get_kernel_name.py
@@ -2,5 +2,5 @@ import subprocess
 
 gcov_name = "gcovkernel' --"
 
-result = subprocess.run(['sudo', 'grub-mkconfig', '|', 'grep', '-iE', "menuentry 'Ubuntu, with Linux", '|', 'awk', '{print i++ " : "$1, $2, $3, $4, $5, $6, $7}'], stdout=subprocess.PIPE)
-print(result.stdout)
+output = subprocess.getoutput("""sudo grub-mkconfig | grep -iE "menuentry 'Ubuntu, with Linux" | awk '{print i++ " : "$1, $2, $3, $4, $5, $6, $7}'""")
+print(output)
