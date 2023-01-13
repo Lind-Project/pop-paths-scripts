43e280f944c97a1ace6c990f56533dc222e6e75d
Tristan Brigham <67669475+TristanB22@users.noreply.github.com>
2023-01-13 16:50:40 -0700

    Update the timing for login

--- host_scripts/ssh_command.sh
+++ host_scripts/ssh_command.sh
@@ -9,8 +9,14 @@ then
     # generate the key for logging into the server
     ssh-keygen -N "" -f /home/tbrigham/key <<<y
 
+    # wait for a moment
+    sleep 10
+
     # ssh into the kvm, download this repo, set up app armor, and install the libraries that we need
     sudo sshpass -p "password" ssh-copy-id -i /home/tbrigham/key.pub -p 2222 -o StrictHostKeyChecking=no ubuntu@localhost
+    
+    # wait for a moment for the key to register
+    sleep 20
     sudo ssh -i /home/tbrigham/key -o StrictHostKeyChecking=no -p 2222 ubuntu@localhost "git clone https://github.com/Lind-Project/pop-paths-scripts.git && bash pop-paths-scripts/pop_paths_scripts/script1.sh"
 
 elif [ $1 -eq 2 ]
