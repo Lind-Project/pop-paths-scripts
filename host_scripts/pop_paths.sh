3e0d2f64f75769c44ad4fb65adf22ffe50ec7619
Tristan Brigham <67669475+TristanB22@users.noreply.github.com>
2023-01-16 15:59:51 -0500

    small updates to make trinity work

--- host_scripts/pop_paths.sh
+++ host_scripts/pop_paths.sh
@@ -10,8 +10,3 @@ echo ""
 echo ""
 sudo bash ./ssh_command.sh 4
 
-echo ""
-echo "DELETING KVM AND CREATED FILES"
-echo ""
-echo ""
-sudo rm -f /hdd/jammy-server-cloudimg-amd64.img  || true
--- host_scripts/run_client.sh
+++ host_scripts/run_client.sh
@@ -1,5 +1,16 @@
+# wait for the vm to initialize
 sleep 300 
+
+# run the setup script
 cd host_scripts 
 bash ./setup.sh 
+
+# wait for the vm to restart and get all of the data
 sleep 300 
 bash ./pop_paths.sh 
+
+echo ""
+echo "DELETING KVM AND CREATED FILES"
+echo ""
+echo ""
+sudo rm -f /hdd/jammy-server-cloudimg-amd64.img  || true
