57d09798c8c879fbeef4de427f6bda40116a62ac
lind <lind@nyu.edu>
2023-02-06 02:59:21 +0000

    cve and path line scripts and jsons

---
+++ cve_scripts/cve_scraper.py
@@ -0,0 +1,45 @@
+import sys, getopt, os
+import wget
+import json, requests
+from selenium import webdriver
+from bs4 import BeautifulSoup
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+#uncomment these two lines to insert any kernel version from command line or just change url manually
+#kern = sys.argv[1]
+#url = f"https://www.linuxkernelcves.com/steams/{kern}"
+url = "https://www.linuxkernelcves.com/streams/5.15"
+
+options = webdriver.ChromeOptions()
+options.add_argument('--headless')
+driver = webdriver.Chrome()
+driver.get(url)
+
+html = driver.page_source
+soup = BeautifulSoup(html, "html.parser")
+
+cards = soup.find_all("div", class_="cve-card v-card v-sheet theme--light")
+
+dict = {}
+for card in cards:
+    cve_date = card.find("span",class_="headline").text.strip()
+    kern_hash = card.find("span",class_="mono").text.strip()
+    blurb = card.find("p").text.strip()
+    dict[cve_date] = (kern_hash, blurb)
+with open("cve_dict.json", "w") as f:
+    json.dump(dict, f)
+
+for date, hash_num in dict.items():
+    ids = hash_num[0]
+    if ids == "":
+        continue
+    cve_date = date
+    url2 = f"https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/patch/?id={ids}"
+    dir = fr'\Users\jkoer\Desktop\Cooper\lind_cont\diff-{kern}'
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+    filepath = fr'{dir}\{cve_date}.txt'
+    wget.download(url2, filepath) 
+
---
+++ cve_scripts/paths2json.py
@@ -0,0 +1,26 @@
+import sys, getopt, os, json
+
+#this is just a temporary path name, just uncomment the two lines below to use a command line argument for the pathname
+#pathname = sys.argv[1]
+pathname = r'\Users\jkoer\Desktop\Cooper\lind_cont\pop_paths_data.txt'
+filename = os.path.basename(pathname)
+
+path_lines = {}
+result = ""
+with open(filename) as file:
+    for line in file:
+        stripped_line = line.strip()
+        if stripped_line.startswith("SF"):
+            components = stripped_line.split("/")
+            if "include" in components:
+                ind = components.index("include")
+            elif "mm" in components:
+                ind = components.index("mm")
+            result = "/".join(components[ind:])
+            path_lines[result] = []
+        elif stripped_line.isdigit():
+            line_number = int(stripped_line)
+            path_lines[result].append(line_number)
+
+with open("path_lines.json", "w") as f:
+    json.dump(path_lines, f)
