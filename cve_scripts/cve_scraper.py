66038253bcf6db1112c6132d20abbef52f5a7b12
lind <100324413+justyoyo123@users.noreply.github.com>
2023-03-02 23:48:48 +0000

    getting rid of unnecessary imports

--- cve_scripts/cve_scraper.py
+++ cve_scripts/cve_scraper.py
@@ -4,8 +4,6 @@ import json, requests
 from selenium import webdriver
 from bs4 import BeautifulSoup
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 #uncomment these two lines to insert any kernel version from command line or just change url manually
 #kern = sys.argv[1]
