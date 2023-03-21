1431d2a3e40a420a567fbf46665c732df4c4121b
lind <100324413+justyoyo123@users.noreply.github.com>
2023-03-21 00:11:59 +0000

    edits

--- cve_scripts/report.py
+++ cve_scripts/report.py
@@ -1,8 +1,7 @@
 import os
 import json
-directory = r'C:\Users\jkoer\Desktop\Cooper\lind_cont\diff-5.15\\'
-result = ""
-with open (r'C:\Users\jkoer\Desktop\Cooper\lind_cont\final_dict_515.json', 'r') as file:
+#add path to the final dictionary
+#with open (r'', 'r') as file:
     final_dict  = json.load(file)
 
 with open('final_dict_readable.txt', 'w') as f: 
